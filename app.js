863c5c0ed8b46c0bbd75f2201029377037441eca
Tushar Kapil <92357039+Tushar-Kapil@users.noreply.github.com>
2024-03-18 12:24:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 
-const PORT = 5003;
+const PORT = 5005;
 
 const listener = app.listen(PORT, "0.0.0.0", () => {
   console.info(`API gateway listening on port ${listener.address().port}`);
