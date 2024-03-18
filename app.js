eb0f057e9705d99c913ced3866307f5842ffc2ba
Tushar Kapil <92357039+Tushar-Kapil@users.noreply.github.com>
2024-03-18 11:39:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const accessEnv = require("./accessEnv");
+const cors = require("cors");
+
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+app.use("/", indexRouter);
+
+const PORT = 8000;
+
+const listener = app.listen(PORT, "0.0.0.0", () => {
+  console.info(`API gateway listening on port ${listener.address().port}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res) {
+  // const { data } = req.body;
+  const data = ["A", "ABcD", "DOE"];
+
+  const capital_alphabets_array = data
+    .map((x) => x.toUpperCase())
+    .filter((x) => x != parseInt(x));
+
+  const odd_numers_array = data
+    .map((x) => parseInt(x))
+    .filter((x) => x % 2 !== 0)
+    .filter((x) => x == parseInt(x));
+
+  const even_numbers_array = data
+    .map((x) => parseInt(x))
+    .filter((x) => x % 2 === 0);
+
+  const result = {
+    is_success: true,
+    user_id: "tushar_kapil_2110991462",
+    email: "tushar1462.be21@chitkara.edu.in",
+    roll_number: "2110991462",
+    odd_numers_array,
+    even_numbers_array,
+    capital_alphabets_array,
+  };
+
+  return res.status(200).json(result);
+});
+
+module.exports = router;
